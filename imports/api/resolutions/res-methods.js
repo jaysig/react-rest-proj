e6fff2a2c24db6d5c00200dc99d9eb97348876e6
Jaysig <jasontsigmon@gmail.com>
2016-04-27 10:06:54 -0500

    added check security

--- imports/api/resolutions/res-methods.js
+++ imports/api/resolutions/res-methods.js
@@ -2,6 +2,7 @@ import Resolutions from './resolutions.js'
 
 Meteor.methods({
   addResolution(resolution) {
+    check(resolution, String);
     if(!Meteor.userId()) {
       throw new Meteor.Error('not-authorized');
     }
@@ -13,6 +14,7 @@ Meteor.methods({
     });
   },
   toggleResolution(resolution) {
+    check(resolution, Object);
     if(Meteor.userId() !== resolution.user) {
       throw new Meteor.Error('not-authorized');
     }
@@ -21,6 +23,7 @@ Meteor.methods({
     });
   },
   deleteResolution(resolution) {
+    check(resolution, Object);
     if(!Meteor.userId() !== resolution.user) {
       throw new Meteor.Error('not-authorized');
     }
